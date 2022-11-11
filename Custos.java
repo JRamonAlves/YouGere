6b7e93076aeaeccd3ba1e0c41724655eacf098b5
LukeRivia <95368707+LukeRivia@users.noreply.github.com>
2022-11-11 18:09:39 -0300

    Update Custos.java

--- Custos.java
+++ Custos.java
@@ -1,19 +1,19 @@
 public class Custos {
     private String nomeCusto;
     private int valorCusto;
-    
+
+    public Custos(String nomeCusto, int valorCusto) {
+        this.nomeCusto = nomeCusto;
+        this.valorCusto = valorCusto;
+    }
+
     public String getNomeCusto() {
         return nomeCusto;
-    }
-    public void setNomeCusto(String nomeCusto) {
-        this.nomeCusto = nomeCusto;
     }
     public int getValorCusto() {
         return valorCusto;
     }
-    public void setValorCusto(int valorCusto) {
-        this.valorCusto = valorCusto;
-    }
+    
 }
 
 
