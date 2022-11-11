07741a832c76f566a6bf0a085f2061455beaeee6
LukeRivia <95368707+LukeRivia@users.noreply.github.com>
2022-11-11 18:10:32 -0300

    Update Apartamento.java

--- Apartamento.java
+++ Apartamento.java
@@ -4,7 +4,7 @@ import java.util.ArrayList;
 public class Apartamento {
 
     private  ArrayList <ColegasDeQuarto> Colegas = new ArrayList< > (); // O Arrays list do tipo ColegasDequarto recebe colegas, já instanciando a classe "ColegasDeQuarto" como  novo array.
-    private ArrayList <Custos> custo = new ArrayList< > ();             // Custo total para manter apartamento. 
+    private ArrayList <Custos> custos = new ArrayList< > ();             // Custo total para manter apartamento. 
 
 
     // Set's e get's PADRÃO
@@ -18,20 +18,37 @@ public class Apartamento {
         Colegas.add(colega); 
     }
 
+    public void setCusto(Custos custo) {
+        custos.add(custo);
+    }
+
     public ArrayList<Custos> getCusto() {
-        return custo;
+        return custos;
     }
 
     
     // METODOS 
-    public void moradores(){
+    public void Exibirmoradores(){
         // Foreach - para cada obj dentro de colega de quarto me mostre cada um. 
         int i = 1; // Var aux para enumerar cada OBJ que será exibido 
-
         for(ColegasDeQuarto e: Colegas){
-            System.out.println("\n\t" +i+"º - Colega: "+ e.getNome());
+            System.out.println("\n" +i+"º - Colega: "+ e.getNome());
             i += 1;
         }
+    }
+
+    public void ExibirCustos(){
+    // Foreach - para cada obj dentro de colega de quarto me mostre cada um. 
+    int i = 1; // Var aux para enumerar cada OBJ que será exibido 
+    double soma = 0;
+
+        for(Custos e: custos){
+            System.out.println("\n" +i+" * Tipo de despesa: "+ e.getNomeCusto()+ "\n  * Valor da despesa: R$"+e.getValorCusto());
+            soma += e.getValorCusto();
+            i += 1;
+        }
+
+    System.out.println("\n    Total de despesas: R$" +soma);
     }
   
     
