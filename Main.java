29092d9483b60c875f19dc1c7475d82a27e2a273
LukeRivia <95368707+LukeRivia@users.noreply.github.com>
2022-11-17 11:37:01 -0300

    Comment
    Comentário na class MAIN

--- Main.java
+++ Main.java
@@ -1,12 +1,16 @@
 public class Main {
     public static void main(String [] arsgs) {
     Apartamento ap1 = new Apartamento();
+    
+    // COLEGAS DE QUARTO - CRIAR OBJETO PASSANDO AS INFOS EM UM CONSTRUTOR 
     ColegasDeQuarto coleguinha = new ColegasDeQuarto("Lukas", 872087798, 1);
-     ColegasDeQuarto coleguinha2 = new ColegasDeQuarto("Ramon", 872087758, 2);
+    ColegasDeQuarto coleguinha2 = new ColegasDeQuarto("Ramon", 872087758, 2);
+
+    // DA MESMA FORMA OS GASTOS
     Custos moradia = new Custos("Aluguel", 500);
     Custos Comodidade = new Custos("Net/Agua/Luz", 200);
      
-
+    
     ap1.setColegas(coleguinha);
     ap1.setColegas(coleguinha2);
     ap1.Exibirmoradores();
