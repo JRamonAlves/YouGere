4a55f1ca2aaf79c5a297b605ef2eb8e8fe0311a7
LukeRivia <95368707+LukeRivia@users.noreply.github.com>
2022-11-17 22:29:34 -0300

    Changes

--- Main.java
+++ Main.java
@@ -1,12 +1,14 @@
 public class Main {
     public static void main(String [] arsgs) {
+    
+    // OBJ APARTAMENTO.
     Apartamento ap1 = new Apartamento();
     
-    // COLEGAS DE QUARTO - CRIAR OBJETO PASSANDO AS INFOS EM UM CONSTRUTOR 
+    // COLEGAS DE QUARTO - CRIAR OBJETO PASSANDO AS INFOS EM UM CONSTRUTOR.
     ColegasDeQuarto coleguinha = new ColegasDeQuarto("Lukas", 872087798, 1);
     ColegasDeQuarto coleguinha2 = new ColegasDeQuarto("Ramon", 872087758, 2);
 
-    // DA MESMA FORMA OS GASTOS
+    // DA MESMA FORMA OS GASTOS.
     Custos moradia = new Custos("Aluguel", 500);
     Custos Comodidade = new Custos("Net/Agua/Luz", 200);
      
@@ -16,8 +18,9 @@ public class Main {
     ap1.Exibirmoradores();
 
     ap1.setCusto(moradia);
+    System.out.println(ap1.getDespesaTotal());
     ap1.setCusto(Comodidade);
-
-    ap1.ExibirCustos();
+    // ap1.ExibirCustos();
+    System.out.println(ap1.getDespesaTotal());
     }
 }
