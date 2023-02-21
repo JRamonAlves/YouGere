cae12f110360c32f7e859de4f9f7f89b7f546bb1
Lukas Félix <95368707+LukeRivia@users.noreply.github.com>
2023-02-21 11:49:43 -0300

    Update Teste_vitor.java

--- Teste_vitor.java
+++ Teste_vitor.java
@@ -1,10 +1,13 @@
+import javax.swing.*;  
 public class Teste_vitor {
-    // NÃO MEXER **** FEITO POR LUCAS - VOU USAR ISSO PRA APRIMORAR MAIS AINDA... PFV N APAGUE NEM MEXA
-        // String nome;
-        // StringBuilder mensagem = new StringBuilder();
-        // nome = JOptionPane.showInputDialog("Digite seu nome:");
-        // mensagem.append("Bem-vindo ").append(nome).append("!");
-        // JOptionPane.showMessageDialog(null, mensagem);
-        // NÃO MEXER ****
-        // nome = JOptionPane.showInputDialog("Bem-vindo ao YouGere\n| O que deseja fazer? Digite o número correspondente:\n[1] Criar um novo apartamento para gerenciar\n[2] Acessar algum apartamento existente");
+    public static void main(String [] arsgs) {
+
+        String nome;
+StringBuilder mensagem = new StringBuilder();
+// nome = JOptionPane.showInputDialog("Digite seu nome:");
+// mensagem.append("Bem-vindo ").append(nome).append("!");
+// JOptionPane.showMessageDialog(null, mensagem);
+nome = JOptionPane.showInputDialog("Bem-vindo ao YouGere\n| O que deseja fazer? Digite o número correspondente:\n[1] Criar um novo apartamento para gerenciar\n[2] Acessar algum apartamento existente");
+
+}
 }
